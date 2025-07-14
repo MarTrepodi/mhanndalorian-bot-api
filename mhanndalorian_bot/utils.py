f91f01e410ab9ecdf8258dc263e15cfbe3d2caf6
MarTrepodi <martrepodi@gmail.com>
2025-07-14 15:59:52 -0400

    feat(utils): add utility to generate TW opponent URL
    Implemented `get_tw_opponent_url` to construct URLs for opponent guild profiles from given TW data. Added type validation and error handling to ensure robustness when extracting necessary guild ID information.

--- mhanndalorian_bot/utils.py
+++ mhanndalorian_bot/utils.py
@@ -64,3 +64,34 @@ def calc_tw_score_total(zone_status_list: list) -> int:
         raise TypeError("'zone_status' must be a list")
 
     return sum([int(score['zoneStatus']['score']) for score in zone_status_list])
+
+
+def get_tw_opponent_url(tw_data: dict) -> str:
+    """
+    Generates and returns the URL for the opponent guild profile in a Territory War event.
+
+    This method extracts the opponent's guild ID from the provided `tw_data` dictionary,
+    validates its presence, and constructs a URL to their profile hosted on swgoh.gg. It
+    is intended to handle data structures specific to game-related data in the scope of
+    Territory War events.
+
+    Args:
+        tw_data (dict): A dictionary containing Territory War event information, which includes
+            data about the participant guilds and their profiles.
+
+    Returns:
+        str: A string containing the constructed URL to the opponent guild's profile.
+
+    Raises:
+        TypeError: If the provided `tw_data` is not of type dictionary.
+        ValueError: If the necessary 'awayGuild' profile information is missing from `tw_data`.
+    """
+    if not isinstance(tw_data, dict):
+        raise TypeError("'tw_data' must be a dictionary")
+
+    guild_id = tw_data.get('awayGuild', {}).get('profile', {}).get('id')
+
+    if not guild_id:
+        raise ValueError("'tw_data' does not contain 'awayGuild' profile information.")
+
+    return f"https://swgoh.gg/g/{guild_id}/"
